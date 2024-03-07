eda9112278a7fe17d219c27a1ada392407dae148
Koushik Kumar Bug <koushikbug123@gmail.com>
2024-03-08 02:48:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,7 @@
+//db.js
 const mongoose = require("mongoose");
 
 mongoose
-  // .connect(
-  //   "mongodb+srv://koushik:123456789@food-api-cluster.rsmirkc.mongodb.net/?retryWrites=true&w=majority&appName=Food-API-Cluster"
-  // )
   .connect("mongodb://localhost:27017/foodDB")
   //
   //useNewUrlParser , useUnifiedTopology and useIndexparser all these have become obsolete after mongodb 5.0   , {
