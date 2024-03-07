8e668709e029ef39434406fa4a0c7eda2b647660
Koushik Kumar Bug <koushikbug123@gmail.com>
2024-03-08 01:27:45 +0530

    Update foodController.js

--- foodController.js
+++ foodController.js
@@ -5,7 +5,6 @@ exports.createFood = async (req, res) => {
   try {
     const food = new Food(req.body);
     await food.save();
-    //food.save();
     res.status(201).json(food);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -15,8 +14,7 @@ exports.createFood = async (req, res) => {
 // Get all food items
 exports.getAllFood = async (req, res) => {
   try {
-    //const foods = await Food.find();
-    const foods = Food.find();
+    const foods = await Food.find();
     res.json(foods);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -26,8 +24,7 @@ exports.getAllFood = async (req, res) => {
 // Get a specific food item by ID
 exports.getFoodById = async (req, res) => {
   try {
-    //const food = await Food.findById(req.params.id);
-    const food = Food.findById(req.params.id);
+    const food = await Food.findById(req.params.id);
     if (food == null) {
       return res.status(404).json({ message: "Food not found" });
     }
@@ -40,8 +37,7 @@ exports.getFoodById = async (req, res) => {
 // Update a food item
 exports.updateFood = async (req, res) => {
   try {
-    //const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
-    const food = Food.findByIdAndUpdate(req.params.id, req.body, {
+    const food = await Food.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
     res.json(food);
@@ -53,8 +49,7 @@ exports.updateFood = async (req, res) => {
 // Delete a food item
 exports.deleteFood = async (req, res) => {
   try {
-    //await Food.findByIdAndDelete(req.params.id);
-    Food.findByIdAndDelete(req.params.id);
+    await Food.findByIdAndDelete(req.params.id);
     res.json({ message: "Food deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
